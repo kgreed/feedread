442702d66746e41a4f72fe67a3c5b64b3b221659
Kirsten Greed <kirsteng@jobtalk.com.au>
2019-11-05 12:22:12 +1100

    load list

---
+++ FeedRead/FeedRead/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FeedRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = new System.Windows.Forms.ColumnHeader();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(594, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(10, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(710, 667);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Name
+            // 
+            this.Name.Name = "Name";
+            this.Name.Text = "Episode";
+            this.Name.Width = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            //this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+    }
+}
+
---
+++ FeedRead/FeedRead/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace FeedRead
+{
+    public partial class Form1 : Form
+    {
+        private SyndicationFeed feed;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //LoadList();
+            if (listView1.SelectedItems.Count != 1) return;
+            var li = listView1.SelectedItems[0];
+            var subject = li.Text;
+            var item = feed.Items.FirstOrDefault(x => x.Summary.Text == subject);
+            if (item == null) return;
+
+            Debug.Print(item.BaseUri.AbsoluteUri);
+
+        }
+
+        private void LoadList()
+        {
+            const string url = "https://feeds.simplecast.com/JGE3yC0V";
+            var reader = XmlReader.Create(url);
+            feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            listView1.View = View.Details;
+            var i = 0;
+            foreach (SyndicationItem item in feed.Items)
+            {
+                i++;
+                var subject = item.Title.Text;
+                var summary = item.Summary.Text;
+                var lvi = new ListViewItem(subject);
+                //var lvi = new ListViewItem
+                //{
+                //    Text = subject,
+                //    Name = subject,
+                //    ImageKey = $"key{i}"
+                //};
+                listView1.Items.Add(lvi);
+            }
+
+            // MessageBox.Show($"There are {i} items");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadList();
+            //listView1.Refresh();
+        }
+    }
+}
